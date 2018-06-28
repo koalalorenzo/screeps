380ee836c7373964f1cf9b87bab1b0a9dabec6ec
Lorenzo Setale <lorenzo@setale.me>
2018-06-28 20:33:25 +0200

    builders will repair

--- role.builder.js
+++ role.builder.js
@@ -11,10 +11,16 @@ module.exports = {
     /** @param {Creep} creep **/
     run: function(creep) {
         this.initMemory(creep)
-        
+
         // Run only if we have targets, otherwise be a upgrader
         let toBuild = creep.room.find(FIND_CONSTRUCTION_SITES);
-        if(!toBuild.length) {
+        let toRepair = creep.room.find(FIND_STRUCTURES, {
+            filter: function(object){
+                return object.hits < object.hitsMax;
+            }
+        })
+
+        if(!toBuild.length || !toRepair.length) {
             return roleUpgrader.run(creep)
         }
 
@@ -26,12 +32,18 @@ module.exports = {
         }
 
         if(creep.memory.building) {
-            console.log("Creeper building...")
-            if(creep.build(toBuild[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(toBuild[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            // Check if we have anything to build
+            if(toBuild.length){
+                if(creep.build(toBuild[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(toBuild[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            // otherwise check for repairs
+            }else{
+                if(creep.repair(toRepair[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(toRepair[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
         }else{
-            console.log("Creeper getting sources...")
             let source = resource.getSource(creep, 2);
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
